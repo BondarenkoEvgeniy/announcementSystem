d6fdb4c298233a8949119e7a9e751b84e880e9de
EvgenuyBondarenko <ktr2004@mail.ru>
2021-11-29 14:02:25 +0300

    Initial commit

--- src/main/java/com/senla/api/service/IPaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.senla.api.service;
-
-import com.senla.model.dto.PaymentDto;
-
-public interface IPaymentService {
-    void createPayment(PaymentDto paymentDto);
-
-    void addPaymentToAnnouncement(Long id, PaymentDto paymentDto);
-}
--- src/main/java/com/senla/api/service/IRatingService.java
+++
@@ -1,9 +0,0 @@
-package com.senla.api.service;
-
-import com.senla.model.dto.RatingDto;
-
-public interface IRatingService {
-    void giveRatingToUser(Long id, RatingDto ratingDto);
-
-    void createRating(RatingDto ratingDto);
-}
